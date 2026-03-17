011dfd6e71b7456e0eb6efc4f5fc6a6cf778a5c4
dogmania <hj296914@gmail.com>
2026-03-17 21:26:11 +0900

    ✨ Feat: FeedRepository 인터페이스 및 구현체 정의

---
+++ lib/features/feed/data/repository/feed_repository_impl.dart
@@ -0,0 +1,15 @@
+import '../../domain/entity/video_item.dart';
+import '../../domain/repository/feed_repository.dart';
+import '../datasource/mock_video_datasource.dart';
+
+class FeedRepositoryImpl implements FeedRepository {
+  FeedRepositoryImpl(this._dataSource);
+
+  final MockVideoDataSource _dataSource;
+
+  @override
+  Future<List<VideoItem>> getFeedVideos() async {
+    final models = await _dataSource.fetchVideos();
+    return models;
+  }
+}
