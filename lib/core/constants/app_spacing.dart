d071c01bb32c0ce7b20b9469f830c1e335a1afa7
dogmania <hj296914@gmail.com>
2026-03-17 21:04:24 +0900

    ✨ Feat: spacing 정의

---
+++ lib/core/constants/app_spacing.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+abstract final class AppSpacing {
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 12;
+  static const double lg = 16;
+  static const double xl = 24;
+  static const double xxl = 32;
+
+  static const EdgeInsets screenHorizontal = EdgeInsets.symmetric(horizontal: lg);
+  static const EdgeInsets screenPadding = EdgeInsets.all(lg);
+}
