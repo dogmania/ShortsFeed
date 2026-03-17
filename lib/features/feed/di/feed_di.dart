589414c6886e8fcfbdb243230caf8a5489b51fc4
dogmania <hj296914@gmail.com>
2026-03-17 22:22:55 +0900

    ♻️ Refactor: 파일명 변경

--- lib/features/feed/di/feed_di.dart
+++ lib/features/feed/di/feed_di.dart
@@ -1,14 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../data/datasource/mock_video_datasource.dart';
+import '../../data/datasource/feed_data_source.dart';
+import '../../data/datasource/mock_feed_data_source.dart';
 import '../../data/repository/feed_repository_impl.dart';
 import '../../domain/repository/feed_repository.dart';
 
-final mockVideoDataSourceProvider = Provider<MockVideoDataSource>((ref) {
-  return MockVideoDataSource();
+final feedDataSourceProvider = Provider<FeedDataSource>((ref) {
+  return MockFeedDataSource();
 });
 
 final feedRepositoryProvider = Provider<FeedRepository>((ref) {
-  final dataSource = ref.watch(mockVideoDataSourceProvider);
+  final dataSource = ref.watch(feedDataSourceProvider);
   return FeedRepositoryImpl(dataSource);
 });
